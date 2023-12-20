a2b36380994c96fdedebfe65a40a310b626e81ce
mcekov <mtsekov@zohomail.eu>
2023-12-20 09:36:47 +0200

    Add clerk setup for authentication

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { SignedIn, useAuth } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 
 const Page = () => {
+  const { signOut, isSignedIn } = useAuth();
+
   return (
     <View>
       <Text>Profile</Text>
+
+      <Button title="logout" onPress={() => signOut()} />
+      {!isSignedIn && <Link href={"/(modals)/login"}> test </Link>}
     </View>
   );
 };
