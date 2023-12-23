4cec695a98ac4938de31ce74d128a9924c60f312
mcekov <mtsekov@zohomail.eu>
2023-12-23 10:20:32 +0200

    Add single post view

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,7 +1,9 @@
 import { useLocalSearchParams } from "expo-router";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
 import listingsData from "@/assets/data/airbnb-listings.json";
 import Animated from "react-native-reanimated";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const IMG_HEIGHT = 300;
 const { width } = Dimensions.get("window");
@@ -17,6 +19,43 @@ const Page = () => {
           source={{ uri: listing.xl_picture_url }}
           style={styles.image}
         />
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.name}>{listing.name}</Text>
+          <Text style={styles.location}>
+            {listing.room_type} in {listing.smart_location}
+          </Text>
+          <Text style={styles.rooms}>
+            {listing.guests_included} guests · {listing.bedrooms} bedrooms ·
+            {listing.beds} bed · {listing.bathrooms} bathrooms
+          </Text>
+          <View style={{ flexDirection: "row", gap: 4 }}>
+            <Ionicons name="star" size={16} />
+            <Text style={styles.ratings}>
+              {listing.review_scores_rating / 20} · {listing.number_of_reviews}
+              reviews
+            </Text>
+          </View>
+          <View style={styles.divider} />
+
+          <View style={styles.hostView}>
+            <Image
+              source={{ uri: listing.host_picture_url }}
+              style={styles.host}
+            />
+
+            <View>
+              <Text style={{ fontWeight: "500", fontSize: 16 }}>
+                Hosted by {listing.host_name}
+              </Text>
+              <Text>Host since {listing.host_since}</Text>
+            </View>
+          </View>
+
+          <View style={styles.divider} />
+
+          <Text style={styles.description}>{listing.description}</Text>
+        </View>
       </Animated.ScrollView>
     </View>
   );
@@ -25,11 +64,89 @@ const Page = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
   },
   image: {
     height: IMG_HEIGHT,
     width: width,
+  },
+  infoContainer: {
+    padding: 24,
+    backgroundColor: "#fff",
+  },
+  name: {
+    fontSize: 26,
+    fontWeight: "bold",
+    fontFamily: "gotham-m",
+  },
+  location: {
+    fontSize: 18,
+    marginTop: 10,
+    fontFamily: "gotham-m",
+  },
+  rooms: {
+    fontSize: 16,
+    color: Colors.grey,
+    marginVertical: 4,
+    fontFamily: "gotham",
+  },
+  ratings: {
+    fontSize: 16,
+    fontFamily: "gotham-m",
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: Colors.grey,
+    marginVertical: 16,
+  },
+  host: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
+  },
+  hostView: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  footerText: {
+    height: "100%",
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  footerPrice: {
+    fontSize: 18,
+    fontFamily: "mon-sb",
+  },
+  roundButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    color: Colors.primary,
+  },
+  bar: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  header: {
+    backgroundColor: "#fff",
+    height: 100,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.grey,
+  },
+
+  description: {
+    fontSize: 16,
+    marginTop: 10,
+    fontFamily: "mon",
   },
 });
 
